1b51cefeb7b572a9a9f2f33a9ca5d880f21bc7e8
unknown <rahamatshareef.shaik@gmail.com>
2026-06-14 20:56:40 +0530

    Initial blog app setup with home, signup and signin pages

---
+++ models/user.js
@@ -0,0 +1,75 @@
+const { Schema, model } = require('mongoose');
+const { createHmac, randomBytes } = require('crypto');
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    salt: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImageURL: {
+      type: String,
+      default: '/images/default.png',
+    },
+
+    role: {
+      type: String,
+      enum: ['USER', 'ADMIN'],
+      default: 'USER',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', function () {
+  if (!this.isModified('password')) return;
+
+  const salt = randomBytes(16).toString('hex');
+
+  const hashedPassword = createHmac('sha256', salt)
+    .update(this.password)
+    .digest('hex');
+
+  this.salt = salt;
+  this.password = hashedPassword;
+});
+
+userSchema.static('matchPassword', async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const userProvidedHash = createHmac('sha256', user.salt)
+    .update(password)
+    .digest('hex');
+
+  if (user.password !== userProvidedHash) {
+    throw new Error('Invalid password');
+  }
+
+  return user;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
